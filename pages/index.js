8d1d04b6dd088dccf90a0fe1ba3133c40d662df9
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-01-26 12:20:08 +0330

    Create starter template for project

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Ecommerce Website</title>
+        <meta name="description" content="Shop" />
+      </Head>
+    </>
+  );
+};
+
+export default Home;

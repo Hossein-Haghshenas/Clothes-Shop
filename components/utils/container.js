3a39bc463db08fc5e7f338959ddad58029f1f5bf
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-01-27 19:34:45 +0330

    Create reusable container component

---
+++ components/utils/container.js
@@ -0,0 +1,48 @@
+import clsx from "clsx";
+
+const Container = ({
+  children,
+  className,
+  wFull,
+  flex,
+  row,
+  col,
+  justifyCenter,
+  justifyBetween,
+  justifyAround,
+  alignCenter,
+  alignBaseline,
+  gap1,
+  gap2,
+  gap3,
+  wrap,
+  reverse,
+}) => {
+  return (
+    <>
+      <section
+        className={clsx(
+          className && className,
+          wFull && "w-full",
+          flex && "flex",
+          row && "flex-row",
+          col && "flex-col",
+          justifyCenter && "justify-center",
+          justifyBetween && "justify-between",
+          justifyAround && "justify-around",
+          alignCenter && "items-center",
+          alignBaseline && "items-baseline",
+          gap1 && "gap-4",
+          gap2 && "gap-8",
+          gap3 && "gap-12",
+          wrap && "flex-wrap",
+          reverse && "flex-row-reverse",
+        )}
+      >
+        {children}
+      </section>
+    </>
+  );
+};
+
+export default Container;

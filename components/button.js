cf6304474dd9be9e5357339bfe8570fe0dcdf868
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-01-27 19:27:30 +0330

    Create reusable button component

---
+++ components/button.js
@@ -0,0 +1,19 @@
+import clsx from "clsx";
+import { motion } from "framer-motion";
+
+const Button = ({ children, className, type, ...rest }) => {
+  return (
+    <>
+      <motion.button
+        whileHover={{ scale: 1.02 }}
+        type={type ? type : "button"}
+        className={clsx(className ? className : "bg-sky-400 transition ease-in delay-150 hover:bg-sky-300  text-black px-4 py-2 rounded-xl")}
+        {...rest}
+      >
+        {children}
+      </motion.button>
+    </>
+  );
+};
+
+export default Button;

8b89298a1ada23d27f61804b0f74b8cb55c677fd
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-01-28 18:02:28 +0330

    Create index file for make better components address

---
+++ components/index.js
@@ -0,0 +1,7 @@
+export { default as Wrapper } from "./utils/wrapper";
+export { default as Container } from "./utils/container";
+export { default as Banner } from "./utils/banner";
+export { default as Header } from "./header";
+export { default as HeaderBanner } from "./headerBanner";
+export { default as Footer } from "./footer";
+export { default as Button } from "./button";

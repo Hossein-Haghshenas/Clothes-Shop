ef82fa60a7202401317e68f6614c088ac34d5b48
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-01-30 21:48:44 +0330

    Get data and pass to the component & Fix something

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,36 @@
+import { useEffect, useState } from "react";
 import Head from "next/head";
+import { useProductsStore } from "../store/store";
+import { HomeBanners, HomeBlog, HomeExplore, HomeOff, HomePopular } from "../components";
+import { Wrapper } from "../components/utils";
 
 const Home = () => {
+  const [popularProducts, setPopularProducts] = useState([]);
+  const [blogPosts, setBlogPosts] = useState([]);
+  const productsData = useProductsStore().products;
+
+  useEffect(() => {
+    setPopularProducts(productsData.slice(0, 6));
+    setBlogPosts(productsData.slice(10, 13));
+  }, [productsData]);
+
   return (
     <>
       <Head>
-        <title>Ecommerce Website</title>
+        <title>H & N Shop</title>
         <meta name="description" content="Shop" />
       </Head>
+      <Wrapper>
+        <HomeBanners />
+        {/* Explore section */}
+        <HomeExplore />
+        {/* popular products section */}
+        <HomePopular popularData={popularProducts} />
+        {/* off section */}
+        <HomeOff />
+        {/* Our Blog */}
+        <HomeBlog blogData={blogPosts} />
+      </Wrapper>
     </>
   );
 };

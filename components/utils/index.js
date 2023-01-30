614416274a4908ccae7d145f884c22db2dac066a
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-01-30 21:46:47 +0330

    Create index file for utils folder

---
+++ components/utils/index.js
@@ -0,0 +1,5 @@
+// utils
+export { default as Wrapper } from "./wrapper";
+export { default as Container } from "./container";
+export { default as Button } from "./button";
+export { default as Banner } from "./banner";

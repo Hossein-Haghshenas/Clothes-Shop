4401440480ddf80138b609d840bfe4f46fc40557
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-01-30 21:44:34 +0330

    Create home explore section

---
+++ components/home/home.explore.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { Button, Container } from "../utils";
+import exploreSectionImage from "../../public/image/b8.jpg";
+
+const HomeExplore = () => {
+  return (
+    <>
+      <Container className="my-16 mx-8 sm:mx-2 gap-40 lg:gap-20 md:gap-10 sm:gap-4" flex justifyCenter>
+        <section className="w-[30rem]">
+          <Image className="md:h-[23rem]" src={exploreSectionImage} alt="new collections" />
+        </section>
+        <section className="flex flex-col gap-4 mt-16">
+          <h1 className="text-[40px] lg:text-[30px] md:text-[25px] sm:text-[19px] sm:w-15 font-semibold">
+            Explore Our New <br /> Collection
+          </h1>
+          <p className="w-full sm:text-[14px]">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, quas. <br /> Consequatur totam doloribus veniam illo ratione, ipsam
+            harum hic!
+          </p>
+          <section className="w-32">
+            <Button>Explore Now</Button>
+          </section>
+        </section>
+      </Container>
+    </>
+  );
+};
+
+export default HomeExplore;

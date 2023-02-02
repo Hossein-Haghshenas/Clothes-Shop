c6dfd96acb24572fabcd0f685dbfd9f0143fdf74
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-02-02 20:51:57 +0330

    Change something because data changed

--- components/home/home.blog.js
+++ components/home/home.blog.js
@@ -10,16 +10,17 @@ const HomeBlog = ({ blogData }) => {
           <h3 className="font-bold text-[25px]">Our Blog</h3>
         </section>
         <Container flex justifyCenter gap1 wrap>
-          {blogData.map((product) => {
-            const { id, title, description, images } = product;
+          {blogData?.map((product) => {
+            const { id, title, description, image } = product;
+            console.log(product);
 
             return (
               <Container className="w-[20rem] h-[28rem] rounded-lg shadow-xl" flex col gap1 key={id}>
                 <section>
-                  <Image className="rounded-xl" width="400" height="300" src={images[1]} alt={title} />
+                  <Image className="rounded-xl w-full h-[14rem]" width="400" height="300" src={image} alt={title} />
                 </section>
                 <section className="flex justify-around ">
-                  <p>{title}</p>
+                  <p>{truncate(title, { length: 35 })}</p>
                 </section>
                 <section className="flex justify-around px-4 h-[4.5rem]">
                   <p>{truncate(description, { length: 75 })}</p>
--- components/home/home.popular.js
+++ components/home/home.popular.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { truncate } from "lodash";
 import { Container } from "../utils";
 
 const HomePopular = ({ popularData }) => {
@@ -9,15 +10,15 @@ const HomePopular = ({ popularData }) => {
           <h3 className="font-bold text-[25px]">Most Popular Products</h3>
         </section>
         <Container flex justifyCenter gap1 wrap>
-          {popularData.map((product) => {
-            const { id, title, price, images } = product;
+          {popularData?.map((product) => {
+            const { id, title, price, image } = product;
             return (
               <Container className="w-[20rem] h-[19rem] rounded-lg shadow-xl" flex col gap1 key={id}>
                 <section>
-                  <Image className="rounded-xl" width="400" height="300" src={images[1]} alt={title} />
+                  <Image className="rounded-xl w-full h-[16rem]" width="400" height="300" src={image} alt={title} />
                 </section>
                 <section className="flex justify-around">
-                  <p>{title}</p>
+                  <p>{truncate(title, { length: 35 })}</p>
                   <p>${price}</p>
                 </section>
               </Container>

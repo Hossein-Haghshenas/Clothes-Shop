9bbb75e93536c2ae6a8628b1395935f4894fef14
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-02-04 21:16:56 +0330

    Get data from api

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,58 +1,13 @@
-import React from "react";
 import { ProductsBanner, ProductsCategory } from "../../components";
 import { Wrapper } from "../../components/utils";
-import collection from "../../public/image/categories/collection_category.jpg";
-
-const categories = [
-  {
-    id: 0,
-    name: "Collections",
-    image: collection,
-    count: 240,
-  },
-  {
-    id: 1,
-    name: "Shoes",
-    image: collection,
-    count: 240,
-  },
-  {
-    id: 2,
-    name: "Bag",
-    image: collection,
-    count: 240,
-  },
-  {
-    id: 3,
-    name: "Dress",
-    image: collection,
-    count: 240,
-  },
-  {
-    id: 4,
-    name: "Jacket",
-    image: collection,
-    count: 240,
-  },
-  {
-    id: 5,
-    name: "Coat",
-    image: collection,
-    count: 240,
-  },
-  {
-    id: 6,
-    name: "T-Shirt",
-    image: collection,
-    count: 240,
-  },
-];
+import { useProductsStore } from "../../store/store";
 
 const Products = () => {
+  const categoriesData = useProductsStore().categories;
   return (
     <Wrapper>
       <ProductsBanner />
-      <ProductsCategory categories={categories} />
+      <ProductsCategory categories={categoriesData} />
     </Wrapper>
   );
 };

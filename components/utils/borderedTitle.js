26fb9f3ea9c1b6977888f9d1cf2c7a0b3bee498e
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-02-04 21:17:37 +0330

    Create bordered title component

--- components/utils/borderedTitle.js
+++ components/utils/borderedTitle.js
@@ -4,7 +4,7 @@ const BorderedTitle = ({ title, titleClassName, containerClassName }) => {
   return (
     <>
       <section className={clsx("border-b-2 border-slate-300 pb-6 mb-6", containerClassName && containerClassName)}>
-        <h3 className={clsx("font-semibold text-[35px]", titleClassName && titleClassName)}>{title}</h3>
+        <h3 className={clsx("font-semibold text-[35px] sm:text-[25px]", titleClassName && titleClassName)}>{title}</h3>
       </section>
     </>
   );

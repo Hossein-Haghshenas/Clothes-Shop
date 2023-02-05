88435dad2066156f28aa6a07844df6d828934422
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-02-05 23:56:23 +0330

    Change api address

--- api/products.js
+++ api/products.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const getAllProducts = async () => {
-  const baseURL = "https://fakestoreapi.com/products";
+  const baseURL = "https://humorous-perpetual-pipe.glitch.me/products";
   const { data } = await axios.get(baseURL);
   return data;
 };
 
-export const getCategories = async (id) => {
-  const baseURL = `https://fakestoreapi.com/products/categories`;
+export const getCategories = async () => {
+  const baseURL = `https://humorous-perpetual-pipe.glitch.me/categories`;
   const { data } = await axios.get(baseURL);
   return data;
 };

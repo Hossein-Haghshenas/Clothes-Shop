275bd080c1cb2df1cbb1d6168ab6f6d0afcfca5c
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-02-05 23:58:32 +0330

    Import them in index file

--- components/utils/index.js
+++ components/utils/index.js
@@ -4,4 +4,6 @@ export { default as Container } from "./container";
 export { default as Button } from "./button";
 export { default as Banner } from "./banner";
 export { default as BorderedTitle } from "./borderedTitle";
+export { default as Loading } from "./loading";
+export { default as Card } from "./card";
 export { default as FooterMenu } from "./footerMenu";

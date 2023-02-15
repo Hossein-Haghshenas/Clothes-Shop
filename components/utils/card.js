5f88c9530ce1c93f25967b4958fa9bccf0894bfd
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-02-15 12:49:44 +0330

    Update card component

--- components/utils/card.js
+++ components/utils/card.js
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import Image from "next/image";
-import { BiStar, BiHeart } from "react-icons/bi";
+import { AiFillStar, AiOutlineStar, AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import Button from "./button";
 import Container from "./container";
 
@@ -11,13 +12,14 @@ const Card = ({
     rating: { rate, count },
   },
 }) => {
+  const [like, setLike] = useState(false);
   return (
     <>
       <Container className="w-[20rem] border-[1px] border-slate-300 rounded transition-shadow duration-300 hover:shadow-2xl" flex col>
         <section className="relative">
           <Image className="w-full h-[15rem] rounded-t" src={image} width={400} height={400} alt={title} placeholder="blur" blurDataURL={image} />
-          <span className="absolute top-4 right-4 text-white transition-all hover:text-red-600 text-[1.5rem] cursor-pointer">
-            <BiHeart />
+          <span onClick={() => setLike(!like)} className="absolute top-4 right-4 text-[1.5rem] cursor-pointer">
+            {like ? <AiFillHeart className="text-red-600" /> : <AiOutlineHeart className="text-white transition-all hover:text-red-600" />}
           </span>
         </section>
         <section className="p-4">
@@ -28,7 +30,10 @@ const Card = ({
           <Container flex col gap2>
             <span className="flex items-center p-1">
               {[...Array(rate)].map(() => (
-                <BiStar className="text-yellow-400" />
+                <AiFillStar className="text-yellow-400" />
+              ))}
+              {[...Array(5 - rate)].map(() => (
+                <AiOutlineStar className="text-yellow-400" />
               ))}
               <span className="p-2 text-gray-600 text-sm">({count})</span>
             </span>

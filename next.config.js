32527d4fd562f6ddac270ec820a390bfd277cb73
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-02-17 21:07:36 +0330

    Add webpack config for video

--- next.config.js
+++ next.config.js
@@ -4,6 +4,17 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "cdn.glitch.global"],
   },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mp4/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/[hash][ext]",
+      },
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;

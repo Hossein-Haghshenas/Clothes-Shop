8844ac1aed88fcf78a0b07435db1e70395576182
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-02-20 23:16:16 +0330

    Add some props for image component

--- components/utils/banner.js
+++ components/utils/banner.js
@@ -1,14 +1,30 @@
 import Image from "next/image";
 import clsx from "clsx";
 
-const Banner = ({ className, image, imageClassName, text, textClassName, element, width, height, priority, ...rest }) => {
+const Banner = ({
+  className,
+  image,
+  imageWidth,
+  imageHeight,
+  imageClassName,
+  imageClassNameWD,
+  text,
+  textClassName,
+  element,
+  width,
+  height,
+  priority,
+  ...rest
+}) => {
   return (
     <section className={clsx("relative rounded-xl", className && className)} style={{ width: width && width, height: height && height }} {...rest}>
       <Image
-        className={clsx(imageClassName ? imageClassName : "absolute top-0 left-0 w-[100%] h-[100%] rounded-xl")}
+        className={clsx(imageClassName ? imageClassName : "absolute top-0 left-0 w-[100%] h-[100%] rounded-xl", imageClassNameWD && imageClassNameWD)}
         src={image.src}
         alt={image.alt ? image.alt : ""}
         priority={priority ? true : false}
+        width={imageWidth && imageWidth}
+        height={imageHeight && imageHeight}
       />
       <section>
         {text && <p className={clsx(textClassName && textClassName)}>{text && text}</p>}

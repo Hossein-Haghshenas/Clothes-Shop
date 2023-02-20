1f018edab8309cc0c501b078f54deb351abd4e7a
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-02-20 23:18:08 +0330

    Create newsletter component for about page

---
+++ components/about/newsletter.js
@@ -0,0 +1,29 @@
+import { Button, Container } from "../utils";
+import backgroundImg from "../../public/image/about/b14.png";
+
+const Newsletter = () => {
+  return (
+    <>
+      <Container
+        className="bg-[#001f3a] py-12 mt-12 md:flex-col md:gap-8 bg-no-repeat bg-center"
+        flex
+        justifyAround
+        alignCenter
+        style={{ backgroundImage: `url(${backgroundImg.src})` }}
+      >
+        <section className="text-white">
+          <h4 className="text-[22px] font-bold md:text-center">Sign Up For Newsletters</h4>
+          <p className="mt-4 md:text-center">
+            Get E-mails updates about our latest shop and <span className="text-orange-400">special offers</span>
+          </p>
+        </section>
+        <section>
+          <input className="p-2 h-[2.5rem]" type="text" />
+          <Button className="py-2 px-8 bg-sky-600 text-white">Sign Up</Button>
+        </section>
+      </Container>
+    </>
+  );
+};
+
+export default Newsletter;

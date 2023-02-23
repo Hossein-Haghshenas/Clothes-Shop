aebc78da734f237685e56f0ba0e79e90f61c18d7
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-02-23 18:20:48 +0330

    Update login page

--- pages/login.js
+++ pages/login.js
@@ -1,19 +1,34 @@
+import { useRouter } from "next/router";
+import { signIn } from "next-auth/react";
+import { useState } from "react";
 import { BsGoogle } from "react-icons/bs";
 import { Button, Container } from "./../components/utils";
 
 const Login = () => {
+  const { back } = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [result, setResult] = useState({});
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const res = signIn("credentials", { email, password, redirect: false });
+    res.then((res) => setResult(res));
+    result.status === 200 && back();
+  };
+
   return (
-    <>
+    <form onSubmit={submitHandler}>
       <Container className="w-full h-[100vh]" flex justifyCenter alignCenter>
         <Container className="rounded-lg p-4 bg-[#b2aefd]" flex col justifyCenter alignCenter gap1>
           <Container flex col gap2>
-            <input className="px-4 py-2 rounded-lg" type="email" placeholder="Email" />
-            <input className="px-4 py-2 rounded-lg" type="password" placeholder="password" />
+            <input onChange={(e) => setEmail(e.target.value)} className="px-4 py-2 rounded-lg" type="email" placeholder="Email" />
+            <input onChange={(e) => setPassword(e.target.value)} className="px-4 py-2 rounded-lg" type="password" placeholder="password" />
           </Container>
           <span>
             <input type="checkbox" /> Remember me?
           </span>
-          <Button>Login</Button>
+          <Button type="submit">Login</Button>
           <section className="relative flex justify-center w-full">
             <span className="border border-[#1e1e1e] bg-[#b2aefd] px-1 rounded-full z-20">or</span>
             <span className="absolute top-3 left-0 bg-black w-full h-[1px] z-0"></span>
@@ -23,7 +38,7 @@ const Login = () => {
           </section>
         </Container>
       </Container>
-    </>
+    </form>
   );
 };
 

96663be91eb0b10bbf8ecfec565d9e3f10e44b25
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-02-23 18:13:30 +0330

    Create session provider in app

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
+import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </>
   );
 }

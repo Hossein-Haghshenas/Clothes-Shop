faa9f63465d9b96006d1a386639d7f2f869a7ed6
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-02-23 18:14:15 +0330

    Add font to the project

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
+import { Poppins } from "@next/font/google";
+
+const poppins = Poppins({ subsets: ["latin"], style: ["normal", "italic"], weight: ["100", "300", "400", "700", "900"] });
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <SessionProvider session={pageProps.session}>
-        <Component {...pageProps} />
+        <section className={poppins.className}>
+          <Component {...pageProps} />
+        </section>
       </SessionProvider>
     </>
   );

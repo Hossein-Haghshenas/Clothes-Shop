7e77ea7916b4dc400ded02ed4d8c879b8b06f0cd
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-02-25 20:29:52 +0330

    Implementation show cart functionality in header

--- components/header.js
+++ components/header.js
@@ -7,9 +7,9 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import { ListBulletIcon } from "@heroicons/react/24/solid";
 import { AiOutlineShoppingCart } from "react-icons/ai";
-
 import clsx from "clsx";
 import Button from "./utils/button";
+import Cart from "./cart";
 
 const menu = [
   { name: "Home", url: "/", id: 1 },
@@ -20,60 +20,64 @@ const menu = [
 const Header = () => {
   const { push } = useRouter();
   const [showMenu, setShowMenu] = useState(false);
+  const [openCart, setOpenCart] = useState(false);
   const { data, status } = useSession();
 
   return (
-    <header className="w-full flex justify-between items-center relative bg-sky-200 py-4 px-3 shadow-md">
-      {/* logo */}
-      <section>
-        <span className="font-bold">H & N</span>
-      </section>
-      {/* header menu */}
-      <motion.section
-        className={clsx("block sm:hidden z-10", showMenu && "bg-sky-200 sm:!block absolute top-16 left-0 py-5 w-full")}
-        animate={{ height: showMenu ? "10rem" : "2rem" }}
-      >
-        <ul className={clsx("flex sm:flex-col justify-center items-center gap-4 md:gap-1")}>
-          {menu?.map((item) => (
-            <motion.li
-              className="px-4 md:px-2 py-1 rounded-xl bg-sky-200"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              whileHover={{ backgroundColor: "#9bc8ffff", transition: { duration: 0.5 } }}
-              key={item.id}
-            >
-              <Link href={item.url}>{item.name}</Link>
-            </motion.li>
-          ))}
-        </ul>
-      </motion.section>
-      <section className="flex items-center justify-center gap-3">
+    <>
+      <header className="w-full flex justify-between items-center relative bg-sky-200 py-4 px-3 shadow-md z-20">
+        {/* logo */}
         <section>
-          {status === "unauthenticated" ? (
-            <Button onClick={() => push("/login")}>
-              <span className="pr-2">Login</span> | <span className="pl-2">Signup</span>
-            </Button>
-          ) : (
-            <Button onClick={() => signOut()}>
-              <span className="pr-2">Logout</span>
-            </Button>
-          )}
+          <span className="font-bold">H & N</span>
         </section>
-        <section>
-          <button type="button" className="">
-            <AiOutlineShoppingCart className="w-8 h-8" />
-            <span className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">
-              20
-            </span>
-          </button>
+        {/* header menu */}
+        <motion.section
+          className={clsx("block sm:hidden z-10", showMenu && "bg-sky-200 sm:!block absolute top-16 left-0 py-5 w-full")}
+          animate={{ height: showMenu ? "10rem" : "2rem" }}
+        >
+          <ul className={clsx("flex sm:flex-col justify-center items-center gap-4 md:gap-1")}>
+            {menu?.map((item) => (
+              <motion.li
+                className="px-4 md:px-2 py-1 rounded-xl bg-sky-200"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                whileHover={{ backgroundColor: "#9bc8ffff", transition: { duration: 0.5 } }}
+                key={item.id}
+              >
+                <Link href={item.url}>{item.name}</Link>
+              </motion.li>
+            ))}
+          </ul>
+        </motion.section>
+        <section className="flex items-center justify-center gap-3">
+          <section>
+            {status === "unauthenticated" ? (
+              <Button onClick={() => push("/login")}>
+                <span className="pr-2">Login</span> | <span className="pl-2">Signup</span>
+              </Button>
+            ) : (
+              <Button onClick={() => signOut()}>
+                <span className="pr-2">Logout</span>
+              </Button>
+            )}
+          </section>
+          <section>
+            <button type="button" onClick={() => setOpenCart(!openCart)}>
+              <AiOutlineShoppingCart className="w-8 h-8" />
+              <span className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">
+                20
+              </span>
+            </button>
+          </section>
+          <section>
+            <button type="button" className="hidden sm:flex" onClick={() => setShowMenu(!showMenu)}>
+              <ListBulletIcon className="w-8 h-8" />
+            </button>
+          </section>
         </section>
-        <section>
-          <button type="button" className="hidden sm:flex" onClick={() => setShowMenu(!showMenu)}>
-            <ListBulletIcon className="w-8 h-8" />
-          </button>
-        </section>
-      </section>
-    </header>
+      </header>
+      {openCart ? <Cart /> : ""}
+    </>
   );
 };
 

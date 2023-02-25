2a0bed476751cfad9c4fdda08058b9b347e477fc
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-02-25 20:28:57 +0330

    Create cart component

---
+++ components/cart/index.js
@@ -0,0 +1,20 @@
+import { Container } from "../utils";
+import CartHeader from "./cart.header";
+import CartBody from "./cart.body";
+import CartFooter from "./cart.footer";
+
+const Cart = () => {
+  return (
+    <>
+      <Container className="fixed top-0 left-0 right-0 bottom-0 backdrop-blur w-full h-screen opacity-100 z-[350]" flex justifyCenter alignCenter>
+        <section className="flex flex-col p-4 w-[30rem] h-[30rem] bg-white rounded-xl">
+          <CartHeader />
+          <CartBody />
+          <CartFooter />
+        </section>
+      </Container>
+    </>
+  );
+};
+
+export default Cart;

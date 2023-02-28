53b0a69639ec88a46ea5baaf97cb992855890711
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-02-28 18:58:30 +0330

    Create cart footer component

---
+++ components/cart/cart.footer.js
@@ -0,0 +1,23 @@
+import { useCartStore } from "../../store/store";
+import { Button, Container } from "../utils";
+
+const CartFooter = () => {
+  const cartProducts = useCartStore();
+  console.log(cartProducts);
+
+  return (
+    <>
+      <Container className="border-t pt-3" flex justifyBetween alignCenter>
+        <section>
+          <section>Total : ${cartProducts?.cartTotalAmount}</section>
+          <section>{cartProducts?.cartTotalCount} items</section>
+        </section>
+        <section>
+          <Button>Check Out</Button>
+        </section>
+      </Container>
+    </>
+  );
+};
+
+export default CartFooter;

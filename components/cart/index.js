3e6d1db654477bea168c197d092e906d5af25862
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-03-02 13:07:42 +0330

    Add margin to the cart

--- components/cart/index.js
+++ components/cart/index.js
@@ -7,7 +7,7 @@ const Cart = () => {
   return (
     <>
       <Container className="fixed top-0 left-0 right-0 bottom-0 backdrop-blur w-full h-screen opacity-100 z-[350]" flex justifyCenter alignCenter>
-        <section className="flex flex-col p-4 w-[30rem] h-[30rem] bg-white rounded-xl">
+        <section className="flex flex-col p-4 md:p-2 m-4 w-[30rem] h-[30rem] bg-white rounded-xl">
           <CartHeader />
           <CartBody />
           <CartFooter />

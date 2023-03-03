ea7722519f88c9b2e6ad848d6dc30e042b510d11
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-03-03 19:18:02 +0330

    Fix some problems in home explore in responsive mode

--- components/home/home.explore.js
+++ components/home/home.explore.js
@@ -7,18 +7,18 @@ const HomeExplore = () => {
     <>
       <Container className="my-16 mx-8 sm:mx-2 gap-40 lg:gap-20 md:gap-10 sm:gap-4" flex justifyCenter>
         <section className="w-[30rem]">
-          <Image className="md:h-[23rem]" src={exploreSectionImage} alt="new collections" />
+          <Image className="md:h-[23rem] sm:h-[16rem]" src={exploreSectionImage} alt="new collections" />
         </section>
-        <section className="flex flex-col gap-4 mt-16">
-          <h1 className="text-[40px] lg:text-[30px] md:text-[25px] sm:text-[19px] sm:w-15 font-semibold">
+        <section className="flex flex-col gap-4 sm:gap-3 mt-16 sm:mt-1">
+          <h1 className="text-[40px] lg:text-[30px] md:text-[25px] sm:text-[14.5px] sm:w-15 font-semibold">
             Explore Our New <br /> Collection
           </h1>
-          <p className="w-full sm:text-[14px]">
+          <p className="w-full sm:text-[13.5px]">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, quas. <br /> Consequatur totam doloribus veniam illo ratione, ipsam
             harum hic!
           </p>
           <section className="w-32">
-            <Button>Explore Now</Button>
+            <Button className="sm:text-[13px]">Explore Now</Button>
           </section>
         </section>
       </Container>
--- components/utils/button.js
+++ components/utils/button.js
@@ -7,7 +7,7 @@ const Button = ({ children, className, type, ...rest }) => {
       <motion.button
         whileHover={{ scale: 1.02 }}
         type={type ? type : "button"}
-        className={clsx(className ? className : "bg-sky-400 transition ease-in delay-150 hover:bg-sky-300  text-black px-4 py-2 rounded-xl")}
+        className={clsx(className && className, "bg-sky-400 transition ease-in delay-150 hover:bg-sky-300  text-black px-4 py-2 rounded-xl")}
         {...rest}
       >
         {children}

4023ab23a0d6b5e236d6dec9e9e3c089f0779071
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-03-04 20:51:20 +0330

    Use loading component in home blog section & home popular section

--- components/home/home.blog.js
+++ components/home/home.blog.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { truncate } from "lodash";
-import { Button, Container } from "../utils";
+import { Button, Container, Loading } from "../utils";
 
 const HomeBlog = ({ blogData }) => {
   return (
@@ -9,28 +9,34 @@ const HomeBlog = ({ blogData }) => {
         <section>
           <h3 className="font-bold text-[25px]">Our Blog</h3>
         </section>
-        <Container flex justifyCenter gap3 wrap>
-          {blogData?.map((product) => {
-            const { id, title, description, image } = product;
+        {blogData.length === 0 ? (
+          <>
+            <Loading />
+          </>
+        ) : (
+          <Container flex justifyCenter gap3 wrap>
+            {blogData?.map((product) => {
+              const { id, title, description, image } = product;
 
-            return (
-              <Container className="w-[20rem] sm:w-[17rem] h-[28rem] sm:h-[26.5rem] rounded-lg shadow-xl" flex col gap1 key={id}>
-                <section>
-                  <Image className="rounded-t-xl shadow-md w-full h-[14rem]" width="400" height="300" src={image} alt={title} />
-                </section>
-                <section className="flex justify-around ">
-                  <p>{truncate(title, { length: 35 })}</p>
-                </section>
-                <section className="flex justify-around px-4 h-[4.5rem]">
-                  <p>{truncate(description, { length: 75 })}</p>
-                </section>
-                <section className="flex justify-around">
-                  <Button>See More →</Button>
-                </section>
-              </Container>
-            );
-          })}
-        </Container>
+              return (
+                <Container className="w-[20rem] sm:w-[17rem] h-[28rem] sm:h-[26.5rem] rounded-lg shadow-xl" flex col gap1 key={id}>
+                  <section>
+                    <Image className="rounded-t-xl shadow-md w-full h-[14rem]" width="400" height="300" src={image} alt={title} />
+                  </section>
+                  <section className="flex justify-around ">
+                    <p>{truncate(title, { length: 35 })}</p>
+                  </section>
+                  <section className="flex justify-around px-4 h-[4.5rem]">
+                    <p>{truncate(description, { length: 75 })}</p>
+                  </section>
+                  <section className="flex justify-around">
+                    <Button>See More →</Button>
+                  </section>
+                </Container>
+              );
+            })}
+          </Container>
+        )}
       </Container>
     </>
   );
--- components/home/home.popular.js
+++ components/home/home.popular.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Button, Container, RateStars } from "../utils";
+import { Button, Container, Loading, RateStars } from "../utils";
 
 const HomePopular = ({ popularData }) => {
   return (
@@ -9,30 +9,36 @@ const HomePopular = ({ popularData }) => {
         <section>
           <h3 className="font-bold text-[25px]">Most Popular Products</h3>
         </section>
-        <Container flex justifyCenter gap2 wrap>
-          {popularData?.map((product) => {
-            const {
-              id,
-              title,
-              rating: { rate, count },
-              image,
-              category,
-            } = product;
-            return (
-              <Container className="w-[20rem] sm:w-[15rem] rounded-lg shadow-xl" flex col gap1 key={id}>
-                <section>
-                  <Image className="rounded-t-xl shadow-md w-full h-[16rem] sm:h-[14rem]" width="400" height="300" src={image} alt={title} />
-                </section>
-                <section className="flex justify-around items-center pb-2">
-                  <RateStars rate={rate} count={count} />
-                  <Button className="text-[15px] w-[5rem] h-[2.2rem] !rounded-md">
-                    <Link href={`products/${category}/${id}`}>Details</Link>
-                  </Button>
-                </section>
-              </Container>
-            );
-          })}
-        </Container>
+        {popularData.length === 0 ? (
+          <>
+            <Loading />
+          </>
+        ) : (
+          <Container flex justifyCenter gap2 wrap>
+            {popularData?.map((product) => {
+              const {
+                id,
+                title,
+                rating: { rate, count },
+                image,
+                category,
+              } = product;
+              return (
+                <Container className="w-[20rem] sm:w-[15rem] rounded-lg shadow-xl" flex col gap1 key={id}>
+                  <section>
+                    <Image className="rounded-t-xl shadow-md w-full h-[16rem] sm:h-[14rem]" width="400" height="300" src={image} alt={title} />
+                  </section>
+                  <section className="flex justify-around items-center pb-2">
+                    <RateStars rate={rate} count={count} />
+                    <Button className="text-[15px] w-[5rem] h-[2.2rem] !rounded-md">
+                      <Link href={`products/${category}/${id}`}>Details</Link>
+                    </Button>
+                  </section>
+                </Container>
+              );
+            })}
+          </Container>
+        )}
       </Container>
     </>
   );
